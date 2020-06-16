4add488cf31ced639cbd77f85990a79376f58277
zappingseb <sebastian@mail-wolf.de>
2020-06-16 11:14:17 +0200

    update checks

--- check_checks.R
+++ check_checks.R
@@ -1,5 +1,5 @@
 
-check_log <- readLines(list.files("src", pattern = "check\\.log", recursive = TRUE)[1])
+check_log <- readLines(list.files(".", pattern = "check\\.log", recursive = TRUE)[1])
 if (grepl("WARNING", check_log[length(check_log)]) || 
     grepl("ERROR", check_log[length(check_log)]) ||
     grepl("NOTE", check_log[length(check_log)]) ) {
